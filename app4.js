b5e0059d0e176cd9888418827776f8a61c30c514
RuhailJohn <124070119+RuhailJohn@users.noreply.github.com>
2023-02-06 02:31:24 +0500

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,100 @@
+
+// CHAPTER NO 2
+
+// Question 1
+var a ="username"
+alert(a)
+
+
+// Question 2
+var myname = "Ruhail"
+alert(myname)
+
+
+// Question 3
+var message = "Hello World"
+alert(message)
+
+
+// Question 4a
+var biodata = "Jhone Doe"
+alert(biodata)
+
+// Question 4b
+var biodata = "15 years old"
+alert(biodata)
+
+// Question 4c
+var biodata = "Certified Mobile Applicaion Development"
+alert(biodata)
+
+// Question 5
+var a = "PIZZA"+"\nPIZZ"+"\nPIZ"+"\nPI"+"\nP"
+alert(a)
+
+
+// Question 6
+var email ="My email address is ruhailjohn@gmail.com"
+alert(email)
+
+
+// Question 7
+var book = "I am trying to learn from the Book A smarter \n way to learn JavaScript"
+alert(book)
+ 
+
+// Question 8
+document.write("<h2>Yap! I can write HTML content through JavaScript</h2>")
+
+
+// Question 9
+var b = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(b)
+
+
+// CHAPTER NO 3
+
+// Question 1
+var age = "I am 15 years old"
+alert(age)
+
+
+// Question 2  
+var track = "You have visited this site 14 times"
+alert(track)
+
+
+// Question 3
+document.write("My birth year is 1998 \n Data type of my declared variable is number")
+
+
+// Question 4
+document.write("<strong>John Doe<strong/> ordered <strong>5 T-shirt(s)<strong/> on XYZ clothing store")
+
+
+// CHAPTER NO 4
+
+// Question 1
+var a = "x = 1, y = 2, z = 3" 
+alert(a)
+
+
+// Question 2
+// 5 Legal Variable Names
+var name = "John Doe"
+var age = 30
+var $salary = 50000
+var _address = "San Francisco"
+var totalValue = 100
+
+// 5 Illegal Variable Names 
+// var 1stName = "John Doe"
+// var @age = 30
+// var #salary = 50000
+// var .address = "San Fransico"
+// var /totalValue = 100
+alert("Legal Variable: name, age, $salary, _address, totalValue \n\nIllegal Variables: 1stName, @age, #salary, .address, /totalValue")
+
+
+// Question 3
+document.write("<h3>Rules for Naming JS variables</h3> <br/> Variable names can only contain, numbers, $ and _ . For example : $m_1stVariable<br/>Variable must begin with a letter, $ Or _ . For example : $name, _name or name<br/>Variable names are case  sensitive<br/>Variable names should be JS keywords")
